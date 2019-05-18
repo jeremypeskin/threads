1b0a9c708103776bede8225e1d1dd47a5ba2a2f4
Jeremy Peskin <jeremy.p.peskin@gmail.com>
2019-05-18 17:45:50 -0400

    Removed topic scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_18_213056) do
-
-  create_table "topics", force: :cascade do |t|
-    t.string "title"
-    t.string "description"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
